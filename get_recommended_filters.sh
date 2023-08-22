25e6f7175a109e6237e5154794d5300ae3d6f731
xomtram <102791302+xomtram@users.noreply.github.com>
2023-08-22 12:42:06 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -5,6 +5,7 @@ touch input.csv
 
 # declare an array of urls
 urls=(
+  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
